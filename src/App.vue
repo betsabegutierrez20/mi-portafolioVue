5773651a1c38b3893fe1ee63fa295467edec016f
Betsabé Gutierrez <daianagutierrez2004@gmail.com>
2024-11-09 03:05:29 -0300

    Seccion Proyectos, clase 8

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar.vue';
 import DatosPersonales from './components/DatosPersonales.vue';
 import EducacionComponente from './components/EducacionComponente.vue';
 import ExperienciaComponente from './components/ExperienciaComponente.vue';
+import ProyectosComponente from './components/ProyectosComponente.vue';
 
 </script>
 
@@ -16,13 +17,27 @@ import ExperienciaComponente from './components/ExperienciaComponente.vue';
   </header>
   <main>
     <!--el estilo css de secction se encuentra en base.css-->
-    <section id="educacion"><h2>Educación - Cursos</h2></section>
-    <EducacionComponente/> 
-    <section id="experiencia"><h2>Experiencia</h2></section>
-    <ExperienciaComponente/>
-    <section id="proyectos"><h2>Proyectos</h2></section>
-    <section id="habilidades"><h2>Habilidades</h2></section>
-    <section id="intereses"><h2>Intereses</h2></section>
+    <section id="educacion">
+      <h2>Educación - Cursos</h2>
+      <!--Componente Educación-->
+      <EducacionComponente/>
+    </section>
+    <section id="experiencia">
+      <h2>Experiencia</h2>
+      <!--Componete Experiencia-->
+      <ExperienciaComponente/>
+    </section>
+    <section id="proyectos">
+      <h2>Proyectos</h2>
+      <ProyectosComponente/>
+      <!--Componetes Proyectos-->
+    </section>
+    <section id="habilidades">
+      <h2>Habilidades</h2>
+    </section>
+    <section id="intereses">
+      <h2>Intereses</h2>
+    </section>
   </main>
 </template>
 
